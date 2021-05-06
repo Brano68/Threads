c1ff334b90b16e71dad0942ec71a0c70a2c930e1
Branislav Nebus <branislav.nebus@kosickaakademia.sk>
2021-05-06 09:08:06 +0200

    added Number class

---
+++ src/sk/kosickaakademia/nebus/vymienanie/Number.java
@@ -0,0 +1,14 @@
+package sk.kosickaakademia.nebus.vymienanie;
+
+public class Number {
+
+    private int number = 0;
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+}
